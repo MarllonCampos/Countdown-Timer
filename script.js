60f6021a928754d8061fe02e797bb3cd98459466
MarllonCampos <marlloncampos@outlook.com>
2020-09-11 15:22:08 -0300

    Corrigido erros de inserção de apenas horas

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const phraseEl = document.querySelector('h1')
 const hoursEl = document.querySelector('.hours')
 const minutesEl = document.querySelector('.minutes')
 const secondsEl = document.querySelector('.seconds')
+const body = document.querySelector('body')
 
 
 
@@ -16,31 +17,39 @@ phraseEl.innerHTML = frase
 
 const contador = setInterval(mudar,1000)
 
+function mudar(){ 
 
-function mudar(){
-    console.log('interval');
-    seconds--
     if (hours == 0 && minutes == 0 && seconds == 0){
-        console.log('Entrou');
         clearInterval(contador)
+        const endInformation = document.createElement('h3')
+        endInformation.innerText='Acabou :D'
+        body.appendChild(endInformation)
+
+
     }
 
     if(seconds == 0  && minutes != 0 ){
-        console.log('entrou');
-        minutes--
+        console.log('entrou segunos');
         seconds=60
+        minutes--
     }
-
+    
     if(minutes == 0 && hours != 0){
-        console.log('entrou2');
+        console.log('entrou minutos');
+        minutes=59
+        seconds=60
         hours--
-        minutes=60
-    }
 
+    }
+    
 
     hoursEl.innerHTML = (hours < 10 ? `0${hours}` : hours )+  '<span>Horas</span>'
     minutesEl.innerHTML =  (minutes < 10 ? `0${minutes}` : minutes) + '<span>Minutos</span>'
     secondsEl.innerHTML =  (seconds < 10 ? `0${seconds}`: seconds) + '<span>Segundos</span>'
+
     
+
+    
+    seconds--
 }
 
